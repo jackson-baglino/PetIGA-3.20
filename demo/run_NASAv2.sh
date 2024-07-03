e167eea85c10c52a05034ed2e38c4d79a75a9954
Jackson Baglino <jbaglino@caltech.edu>
2024-07-03 09:59:45 -0700

    Refactor run_NASAv2.sh to update domain sizes and number of elements

--- demo/run_NASAv2.sh
+++ demo/run_NASAv2.sh
@@ -23,8 +23,8 @@ input_dir="/Users/jacksonbaglino/PetIGA-3.20/demo/input/"
 # inputFile=$input_dir"grainReadFile-88_s1-10_s2-21.dat"
 # inputFile=$input_dir"grainReadFile-165_s1-10_s2-30.dat"
 # inputFile=$input_dir"grainReadFile-5_s1-10.dat"
-inputFile=$input_dir"grainReadFile-2.dat"
-# inputFile=$input_dir"grainReadFile-10_s1-10.dat"
+# inputFile=$input_dir"grainReadFile-2.dat"
+inputFile=$input_dir"grainReadFile-10_s1-10.dat"
 # inputFile=$input_dir"grainReadFile-37_s1-10_s2-21.dat"
 
 # Define simulation parameters -------------------------------------------------
@@ -35,21 +35,17 @@ dim=3
 dim=$(echo "$dim" | bc -l)
 
 # Domain sizes
-# Lx=0.35e-03                   # Domain size X -- 5 Grain
-# Ly=0.35e-03                   # Domain size Y -- 5 Grain
-# Lz=2.202e-04                  # Domain size Z -- 5 Grain
+# Lx=488.4e-6                    # Domain size X -- 2 Grain
+# Ly=244.2e-6                    # Domain size Y -- 2 Grain
+# Lz=244.2e-6                    # Domain size Z -- 2 Grain
 
-Lx=488.4e-6                    # Domain size X -- 2 Grain
-Ly=244.2e-6                    # Domain size Y -- 2 Grain
-Lz=244.2e-6                    # Domain size Z -- 2 Grain
+# Lx=0.35e-03                    # Domain size X -- 5 Grain
+# Ly=0.35e-03                    # Domain size Y -- 5 Grain
+# Lz=2.6424e-04                  # Domain size Z -- 5 Grain
 
-# Lx=0.5e-03                    # Domain size X -- 10 Grain
-# Ly=0.5e-03                    # Domain size Y -- 10 Grain
-# Lz=1.101e-04                  # Domain size Z -- 10 Grain
-
-# Lx=0.5e-03                    # Domain size X -- 10 Grain
-# Ly=0.5e-03                    # Domain size Y -- 10 Grain
-# Lz=2.202e-04                  # Domain size Z -- 10 Grain
+Lx=0.5e-03                    # Domain size X -- 10 Grain
+Ly=0.5e-03                    # Domain size Y -- 10 Grain
+Lz=2.202e-04                  # Domain size Z -- 10 Grain
 
 # Lx=2.0e-3                     # Domain size X -- 88 Grain
 # Ly=2.0e-3                     # Domain size Y -- 88 Grain
@@ -65,17 +61,17 @@ Ly=$(echo "$Ly" | bc -l)
 Lz=$(echo "$Lz" | bc -l)
 
 # Number of elements
-# Nx=193                       # Number of elements in X -- 5 Grain
-# Ny=193                       # Number of elements in Y -- 5 Grain
-# Nz=122                        # Number of elements in Z -- 5 Grain
+# Nx=264                        # Number of elements in X -- 2 Grain
+# Ny=132                        # Number of elements in Y -- 2 Grain
+# Nz=132                        # Number of elements in Z -- 2 Grain
 
-# Nx=275                        # Number of elements in X -- 10 Grain
-# Ny=275                        # Number of elements in Y -- 10 Grain
-# Nz=122                        # Number of elements in Z -- 10 Grain
+# Nx=190                       # Number of elements in X -- 5 Grain
+# Ny=190                       # Number of elements in Y -- 5 Grain
+# Nz=143                       # Number of elements in Z -- 5 Grain
 
-Nx=264                        # Number of elements in X -- 2 Grain
-Ny=132                        # Number of elements in Y -- 2 Grain
-Nz=132                        # Number of elements in Z -- 2 Grain
+Nx=275                        # Number of elements in X -- 10 Grain
+Ny=275                        # Number of elements in Y -- 10 Grain
+Nz=122                        # Number of elements in Z -- 10 Grain
 
 # Nx=1100                       # Number of elements in X -- 88 Grain
 # Ny=1100                       # Number of elements in Y -- 88 Grain
@@ -88,6 +84,7 @@ Nz=132                        # Number of elements in Z -- 2 Grain
 # Time parameters
 delt_t=1.0e-4                 # Time step
 t_final=48*60*60              # Final time
+# t_final=1.0e-4                # Final time
 n_out=100                     # Number of output files
 
 
@@ -102,7 +99,7 @@ temp=-30.0                    # Temperature
 
 # Initial temperature gradients
 grad_temp0X=0.0               # Initial temperature gradient X
-grad_temp0Y=1.0e-4            # Initial temperature gradient Y
+grad_temp0Y=3.0            # Initial temperature gradient Y
 grad_temp0Z=0.0               # Initial temperature gradient Z
 
 # Convert scientific notation gradients to decimal using bc if needed
@@ -151,6 +148,7 @@ echo "Queing plotNASA.py"
 
 # Create descriptive file ------------------------------------------------------
 echo "----- SIMULATION PARAMETERS -----" > $folder/sim_params.dat
+echo "Input file: $inputFile" >> $folder/sim_params.dat
 
 echo " " >> $folder/sim_params.dat
 
